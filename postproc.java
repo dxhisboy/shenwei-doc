09ac7e4206f0e23d9bbb395b6a749ff55b923396
Xiaohui Duan <dxhisboy@126.com>
2017-06-04 00:18:23 +0800

    removed unused java

--- postproc.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-public class postproc{
-    public static void main(String []args) throws Exception{
-	File in = new File(args[0]);
-	BufferedReader bfr = new BufferedReader(new FileReader(in));
-	StringBuilder bd, bf;
-    }
-}
